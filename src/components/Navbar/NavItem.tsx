3891d04bd68f430f2075a00e4082e7ad6b9998ce
parsa_nazmi <parsa.nazmi@znu.ac.ir>
2023-01-22 00:58:12 +0330

    feat (font): Adding fontawesome

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -3,7 +3,7 @@ import INavbarProps from "../../interfaces/navbarprops";
 
 function NavItem({ title }: INavbarProps): JSX.Element {
   return (
-    <li className="cursor-pointer  text-xl uppercase text-light-heading hover:text-light-hover dark:text-dark-heading dark:hover:text-dark-hover">
+    <li className="cursor-pointer  text-xl font-bold uppercase text-light-heading hover:text-light-hover dark:text-dark-heading dark:hover:text-dark-hover">
       {title}
     </li>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import User from "./User";
 import DarkMode from "./DarkMode";
 import NavItem from "./NavItem";
+import Search from "./Search";
 
 function Navbar(): JSX.Element {
   return (
-    <nav className="flex items-center justify-center bg-light-secondary h-16 dark:bg-dark-secondary sticky top-0">
+    <nav className="sticky top-0 flex h-16 items-center justify-center bg-light-secondary dark:bg-dark-secondary">
       <div className="flex w-full max-w-7xl items-center justify-between">
         <ul className="flex items-center justify-between gap-4">
-          <NavItem title= "home"/>
-          <NavItem title= "movies"/>
-          <NavItem title= "series"/>
-          <NavItem title= "contact"/>
-          <NavItem title= "about"/>
+          <NavItem title="home" />
+          <NavItem title="movies" />
+          <NavItem title="series" />
+          <NavItem title="contact" />
+          <NavItem title="about" />
         </ul>
-        <div className="flex items-center justify-center gap-2">
-          <a href="">Search</a>
+        <div className="flex items-center justify-center gap-10">
+          <Search />
           <User />
           <DarkMode />
         </div>
