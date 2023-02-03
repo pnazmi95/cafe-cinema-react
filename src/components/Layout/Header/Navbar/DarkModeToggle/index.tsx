9a944f763b36c02ae1eb2b0df7d2e4dc0f841529
parsa_nazmi <parsa.nazmi@znu.ac.ir>
2023-02-04 00:20:13 +0330

    dark mode handled by localstorage

--- src/components/Layout/Header/Navbar/DarkModeToggle/index.tsx
+++ src/components/Layout/Header/Navbar/DarkModeToggle/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faLightbulb } from "@fortawesome/free-regular-svg-icons";
 
@@ -10,16 +10,38 @@ const DarkModeToggle: React.FunctionComponent<IDarkModeToggleProps> = (
   props
 ): JSX.Element => {
   const [toggle, setToggle] = useState(true);
+
+  useEffect(() => {
+    darkModeHandler();
+  }, []);
+
   const darkModeHandler = () => {
-    setToggle(!toggle);
-    toggle
-      ? (document.documentElement.className = "dark")
-      : (document.documentElement.className = "");
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.className = "dark";
+      setToggle(false);
+    } else {
+      document.documentElement.className = "";
+    }
   };
+
+  const toggleHandler = () => {
+    setToggle(!toggle);
+    if (toggle) {
+      localStorage.theme = "dark";
+    } else {
+      localStorage.theme = "light";
+    }
+    darkModeHandler();
+  };
+  
   return (
     <div
       className="cursor-pointer text-3xl text-light-heading hover:text-light-hover dark:text-dark-heading dark:hover:text-dark-hover"
-      onClick={darkModeHandler}
+      onClick={toggleHandler}
     >
       {!toggle ? LIGHT : DARK}
     </div>
